ee6980e219390990bb85119fefc7c42740e5cd3e
CyanMarine <58150571+PrincessCyanMarine@users.noreply.github.com>
2022-07-22 19:11:09 -0300

    Faster Minecarts

--- pages/mods.tsx
+++ pages/mods.tsx
@@ -4,11 +4,18 @@ export default () => (
   <div>
     <h1>Mods</h1>
     <p>
-      <Link href="https://github.com/PrincessCyanMarine/SimpleVeinminer">
+      <Link href="https://www.curseforge.com/minecraft/mc-mods/simple-veinminer">
         <big>Simple veinminer</big>
         <small>external</small>
       </Link>{" "}
       - a veinminer made for the fabric modloader
+    </p>
+    <p>
+      <Link href="https://www.curseforge.com/minecraft/mc-mods/cyans-faster-minecarts/">
+        <big>Faster Minecarts</big>
+        <small>external</small>
+      </Link>{" "}
+      - serverside mod for the fabric modloader that makes minecarts faster
     </p>
   </div>
 );
