25f6ed49b7a93782e47a2f1287bf342b38fbc7b3
CyanMarine <58150571+PrincessCyanMarine@users.noreply.github.com>
2023-03-24 23:37:07 -0300

    Add files via upload

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,13 +5,14 @@ import Layout from "../components/Layout";
 import $ from "jquery";
 import { useEffect } from "react";
 import { useRouter } from "next/router";
+// import CursorDot from "../components/CursorDot";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
 
   useEffect(() => {
     let shouldContinue = () => true;
-    let titles = $("h1, h2, h3, h4, h5, h6");
+    let titles = $("h1, h2, h3, h4, h5, h6").not(".no-animation");
     const applyAnimation = (el: JQuery<HTMLElement>) =>
       setTimeout(() => {
         el.animate(
@@ -42,24 +43,29 @@ function MyApp({ Component, pageProps }: AppProps) {
         <title>The Cyan seas</title>
         <link rel="icon" type="image/x-icon" href="/favicon.ico" />
 
-        <meta property="og:title" content="The Cyan seas" />
-        <meta property="og:site_name" content="The Cyan seas" />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="http://cyanmarine.net" />
-        <meta
-          property="og:image"
-          content="https://www.cyanmarine.net/assets/images/thumb.png"
-        />
+        {!["/japanese/sensei/debt"].includes(router.asPath) && (
+          <>
+            <meta property="og:title" content="The Cyan seas" />
+            <meta property="og:site_name" content="The Cyan seas" />
+            <meta property="og:type" content="website" />
+            <meta property="og:url" content="http://cyanmarine.net" />
+            <meta
+              property="og:image"
+              content="https://www.cyanmarine.net/assets/images/thumb.png"
+            />
+            <meta name="twitter:card" content="summary_large_image" />
+          </>
+        )}
         <meta property="og:author" content="CyanMarine" />
         <meta property="author" content="CyanMarine" />
         <meta name="theme-color" content="#00FFFF" />
-
-        <meta name="twitter:card" content="summary_large_image" />
       </Head>
 
       <Layout>
         <Component {...pageProps} />
       </Layout>
+
+      {/* <CursorDot /> */}
     </div>
   );
 }
