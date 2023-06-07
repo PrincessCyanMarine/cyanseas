697517e65a0f2d69b9a754fce95a3757a89c6509
CyanMarine <58150571+PrincessCyanMarine@users.noreply.github.com>
2023-06-07 13:10:20 -0300

    Ignore ESLint

--- next.config.js
+++ next.config.js
@@ -14,6 +14,12 @@ if (
 const nextConfig = {
   reactStrictMode: true,
 
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+
   async redirects() {
     return [
       {
